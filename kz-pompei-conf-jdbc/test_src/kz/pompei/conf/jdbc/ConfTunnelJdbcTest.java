d824dc485f268a52687113acd37f4abdaefff139
Евгений Колпаков <pompei@mail.ru>
2026-05-18 21:26:01 +0500

    added tests modificationMarker_updatesOnRowChange_withTwoParams ;  read_existsInDbTable_withThreeConfigsAndMultilineComments

--- kz-pompei-conf-jdbc/test_src/kz/pompei/conf/jdbc/ConfTunnelJdbcTest.java
+++ kz-pompei-conf-jdbc/test_src/kz/pompei/conf/jdbc/ConfTunnelJdbcTest.java
@@ -77,6 +77,198 @@ public class ConfTunnelJdbcTest extends JdbcTestDbUtils {
   }
 
   @Test(dataProvider = "databaseType")
+  public void read_existsInDbTable_withThreeConfigsAndMultilineComments(@NonNull DatabaseType databaseType) {
+
+    String nameOfThisMethod = "read_exists_three_cfgs";
+
+    ConnectionGet connectionGet = createConnectionGet(databaseType, nameOfThisMethod);
+
+    ConfTunnelJdbcDef def = new ConfTunnelJdbcDef();
+    def.tableName = nameOfThisMethod + "_" + RND.str(8);
+
+    createTable(connectionGet, def);
+    clearTable(connectionGet, def.tableName);
+
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "first-config.hotconf",
+      "",
+      "",
+      "first config comment 1\nfirst config comment 2\nfirst config comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "first-config.hotconf",
+      "alpha",
+      "alpha-value",
+      "alpha comment 1\nalpha comment 2\nalpha comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "first-config.hotconf",
+      "beta",
+      "beta-value",
+      "beta comment 1\nbeta comment 2\nbeta comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "first-config.hotconf",
+      "gamma",
+      "gamma-value",
+      "gamma comment 1\ngamma comment 2\ngamma comment 3"
+    );
+
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "second-config.hotconf",
+      "",
+      "",
+      "second config comment 1\nsecond config comment 2\nsecond config comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "second-config.hotconf",
+      "delta",
+      "delta-value",
+      "delta comment 1\ndelta comment 2\ndelta comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "second-config.hotconf",
+      "epsilon",
+      "epsilon-value",
+      "epsilon comment 1\nepsilon comment 2\nepsilon comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "second-config.hotconf",
+      "zeta",
+      "zeta-value",
+      "zeta comment 1\nzeta comment 2\nzeta comment 3"
+    );
+
+    insertRow(
+      connectionGet,
+      def,
+      "other/folder",
+      "third-config.hotconf",
+      "",
+      "",
+      "third config comment 1\nthird config comment 2\nthird config comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "other/folder",
+      "third-config.hotconf",
+      "eta",
+      "eta-value",
+      "eta comment 1\neta comment 2\neta comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "other/folder",
+      "third-config.hotconf",
+      "theta",
+      "theta-value",
+      "theta comment 1\ntheta comment 2\ntheta comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "other/folder",
+      "third-config.hotconf",
+      "iota",
+      "iota-value",
+      "iota comment 1\niota comment 2\niota comment 3"
+    );
+
+    ConfTunnelJdbc confTunnelJdbc = ConfTunnelJdbcBuilder.build(connectionGet, def);
+
+    //
+    //
+    Conf firstConf = confTunnelJdbc.read("some/folder/first-config.hotconf");
+    Conf secondConf = confTunnelJdbc.read("some/folder/second-config.hotconf");
+    Conf thirdConf = confTunnelJdbc.read("other/folder/third-config.hotconf");
+    //
+    //
+
+    assertThat(firstConf).isNotNull();
+    assertThat(firstConf.confComments).isEqualTo(List.of(
+      "first config comment 1",
+      "first config comment 2",
+      "first config comment 3"
+    ));
+    ConfParam firstAlpha = firstConf.params.get(0);// params sorted by name
+    ConfParam firstBeta = firstConf.params.get(1);
+    ConfParam firstGamma = firstConf.params.get(2);
+    assertThat(firstAlpha.name).isEqualTo("alpha");
+    assertThat(firstBeta.name).isEqualTo("beta");
+    assertThat(firstGamma.name).isEqualTo("gamma");
+    assertThat(firstAlpha.valueStr).isEqualTo("alpha-value");
+    assertThat(firstBeta.valueStr).isEqualTo("beta-value");
+    assertThat(firstGamma.valueStr).isEqualTo("gamma-value");
+    assertThat(firstAlpha.comments).isEqualTo(List.of("alpha comment 1", "alpha comment 2", "alpha comment 3"));
+    assertThat(firstBeta.comments).isEqualTo(List.of("beta comment 1", "beta comment 2", "beta comment 3"));
+    assertThat(firstGamma.comments).isEqualTo(List.of("gamma comment 1", "gamma comment 2", "gamma comment 3"));
+
+    assertThat(secondConf).isNotNull();
+    assertThat(secondConf.confComments).isEqualTo(List.of(
+      "second config comment 1",
+      "second config comment 2",
+      "second config comment 3"
+    ));
+    ConfParam secondDelta = secondConf.params.get(0);// params sorted by name
+    ConfParam secondEpsilon = secondConf.params.get(1);
+    ConfParam secondZeta = secondConf.params.get(2);
+    assertThat(secondDelta.name).isEqualTo("delta");
+    assertThat(secondEpsilon.name).isEqualTo("epsilon");
+    assertThat(secondZeta.name).isEqualTo("zeta");
+    assertThat(secondDelta.valueStr).isEqualTo("delta-value");
+    assertThat(secondEpsilon.valueStr).isEqualTo("epsilon-value");
+    assertThat(secondZeta.valueStr).isEqualTo("zeta-value");
+    assertThat(secondDelta.comments).isEqualTo(List.of("delta comment 1", "delta comment 2", "delta comment 3"));
+    assertThat(secondEpsilon.comments).isEqualTo(List.of("epsilon comment 1", "epsilon comment 2", "epsilon comment 3"));
+    assertThat(secondZeta.comments).isEqualTo(List.of("zeta comment 1", "zeta comment 2", "zeta comment 3"));
+
+    assertThat(thirdConf).isNotNull();
+    assertThat(thirdConf.confComments).isEqualTo(List.of(
+      "third config comment 1",
+      "third config comment 2",
+      "third config comment 3"
+    ));
+    ConfParam thirdEta = thirdConf.params.get(0);// params sorted by name
+    ConfParam thirdIota = thirdConf.params.get(1);
+    ConfParam thirdTheta = thirdConf.params.get(2);
+    assertThat(thirdEta.name).isEqualTo("eta");
+    assertThat(thirdIota.name).isEqualTo("iota");
+    assertThat(thirdTheta.name).isEqualTo("theta");
+    assertThat(thirdEta.valueStr).isEqualTo("eta-value");
+    assertThat(thirdIota.valueStr).isEqualTo("iota-value");
+    assertThat(thirdTheta.valueStr).isEqualTo("theta-value");
+    assertThat(thirdEta.comments).isEqualTo(List.of("eta comment 1", "eta comment 2", "eta comment 3"));
+    assertThat(thirdIota.comments).isEqualTo(List.of("iota comment 1", "iota comment 2", "iota comment 3"));
+    assertThat(thirdTheta.comments).isEqualTo(List.of("theta comment 1", "theta comment 2", "theta comment 3"));
+  }
+
+  @Test(dataProvider = "databaseType")
   public void read_tableDoesNotExists(@NonNull DatabaseType databaseType) {
 
     String nameOfThisMethod = "read_tableDoesNotExists";
@@ -373,5 +565,82 @@ public class ConfTunnelJdbcTest extends JdbcTestDbUtils {
     assertThat(initialLastModified).isNotNull();
     assertThat(updatedLastModified).isNotNull();
     assertThat(updatedLastModified).isGreaterThan(initialLastModified);
+  }
+
+  @Test(dataProvider = "databaseType")
+  public void modificationMarker_updatesOnRowChange_withTwoParams(@NonNull DatabaseType databaseType) throws InterruptedException {
+
+    String nameOfThisMethod = "modMarker_two_params";
+
+    ConnectionGet connectionGet = createConnectionGet(databaseType, nameOfThisMethod);
+
+    ConfTunnelJdbcDef def = new ConfTunnelJdbcDef();
+    def.tableName = nameOfThisMethod + "_" + RND.str(8);
+
+    createTable(connectionGet, def);
+    clearTable(connectionGet, def.tableName);
+
+    insertRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "first-config.hotconf",
+      "alpha",
+      "alpha-value",
+      "alpha comment 1\nalpha comment 2\nalpha comment 3"
+    );
+    insertRow(
+      connectionGet,
+      def,
+      "other/folder",
+      "second-config.hotconf",
+      "beta",
+      "beta-value",
+      "beta comment 1\nbeta comment 2\nbeta comment 3"
+    );
+
+    ConfTunnelJdbc confTunnelJdbc = ConfTunnelJdbcBuilder.build(connectionGet, def);
+
+    //
+    //
+    Long initialFirstMarker = confTunnelJdbc.modificationMarker("some/folder/first-config.hotconf");
+    Long initialSecondMarker = confTunnelJdbc.modificationMarker("other/folder/second-config.hotconf");
+    //
+    //
+
+    Thread.sleep(1200);
+    updateRow(
+      connectionGet,
+      def,
+      "other/folder",
+      "second-config.hotconf",
+      "beta",
+      "beta-value-changed",
+      "beta comment 1\nbeta comment 2\nbeta comment 3"
+    );
+    updateRow(
+      connectionGet,
+      def,
+      "some/folder",
+      "first-config.hotconf",
+      "alpha",
+      "alpha-value-changed",
+      "alpha comment 1\nalpha comment 2\nalpha comment 3"
+    );
+
+    //
+    //
+    Long updatedFirstMarker = confTunnelJdbc.modificationMarker("some/folder/first-config.hotconf");
+    Long updatedSecondMarker = confTunnelJdbc.modificationMarker("other/folder/second-config.hotconf");
+    //
+    //
+
+    assertThat(initialFirstMarker).isNotNull();
+    assertThat(initialSecondMarker).isNotNull();
+    assertThat(updatedFirstMarker).isNotNull();
+    assertThat(updatedSecondMarker).isNotNull();
+
+    assertThat(updatedFirstMarker).isGreaterThan(initialFirstMarker);
+    assertThat(updatedSecondMarker).isGreaterThan(initialSecondMarker);
   }
 }
