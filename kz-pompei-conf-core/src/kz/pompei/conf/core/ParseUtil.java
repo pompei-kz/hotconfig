8546679fb1050d6841feb085e72a76b4f29c64c2
Евгений Колпаков <pompei@mail.ru>
2026-05-19 18:15:00 +0500

    • Add primitive and boxed type parsing coverage

--- kz-pompei-conf-core/src/kz/pompei/conf/core/ParseUtil.java
+++ kz-pompei-conf-core/src/kz/pompei/conf/core/ParseUtil.java
@@ -6,6 +6,38 @@ import lombok.NonNull;
 public class ParseUtil {
 
   public static Object parseStrToGenericType(String valueStr, @NonNull DynamicParams dynamicParams, @NonNull Type genericReturnType) {
+    if (genericReturnType == String.class) return valueStr;
+    if (valueStr == null) return defaultNullValue(genericReturnType);
+    if (genericReturnType == boolean.class || genericReturnType == Boolean.class) return parseBoolean(valueStr);
+    if (genericReturnType == byte.class || genericReturnType == Byte.class) return Byte.parseByte(valueStr);
+    if (genericReturnType == short.class || genericReturnType == Short.class) return Short.parseShort(valueStr);
+    if (genericReturnType == int.class || genericReturnType == Integer.class) return Integer.parseInt(valueStr);
+    if (genericReturnType == long.class || genericReturnType == Long.class) return Long.parseLong(valueStr);
+    if (genericReturnType == float.class || genericReturnType == Float.class) return Float.parseFloat(valueStr);
+    if (genericReturnType == double.class || genericReturnType == Double.class) return Double.parseDouble(valueStr);
+    if (genericReturnType == char.class || genericReturnType == Character.class) {
+      if (valueStr.length() != 1) throw new IllegalArgumentException("hVMkE7Yv2t :: Cannot parse char from string: " + valueStr);
+      return valueStr.charAt(0);
+    }
     return valueStr;
+  }
+
+  private static boolean parseBoolean(@NonNull String valueStr) {
+    return switch (valueStr.toLowerCase()) {
+      case "t", "true", "1", "on", "yes", "y", "да" -> true;
+      default -> false;
+    };
+  }
+
+  private static Object defaultNullValue(@NonNull Type genericReturnType) {
+    if (genericReturnType == boolean.class) return false;
+    if (genericReturnType == byte.class) return (byte) 0;
+    if (genericReturnType == short.class) return (short) 0;
+    if (genericReturnType == int.class) return 0;
+    if (genericReturnType == long.class) return 0L;
+    if (genericReturnType == float.class) return 0F;
+    if (genericReturnType == double.class) return 0D;
+    if (genericReturnType == char.class) return (char) 0;
+    return null;
   }
 }
--- kz-pompei-conf-core/test_src/kz/pompei/conf/core/ParseUtilTest.java
+++ kz-pompei-conf-core/test_src/kz/pompei/conf/core/ParseUtilTest.java
@@ -1,10 +1,93 @@
 package kz.pompei.conf.core;
 
+import java.lang.reflect.Type;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParseUtilTest {
+
+  @DataProvider
+  public Object[][] primitiveAndBoxedTypes() {
+    return new Object[][]{
+      {"7", byte.class, (byte) 7},
+      {"8", Byte.class, (byte) 8},
+      {"300", short.class, (short) 300},
+      {"301", Short.class, (short) 301},
+      {"123456", int.class, 123456},
+      {"654321", Integer.class, 654321},
+      {"1234567890123", long.class, 1234567890123L},
+      {"3210987654321", Long.class, 3210987654321L},
+      {"3.25", float.class, 3.25F},
+      {"4.5", Float.class, 4.5F},
+      {"6.75", double.class, 6.75D},
+      {"7.125", Double.class, 7.125D},
+      {"x", char.class, 'x'},
+      {"y", Character.class, 'y'},
+    };
+  }
+
+  @DataProvider
+  public Object[][] booleanTrueValues() {
+    return new Object[][]{
+      {"t"},
+      {"T"},
+      {"true"},
+      {"TRUE"},
+      {"TrUe"},
+      {"1"},
+      {"on"},
+      {"ON"},
+      {"On"},
+      {"yes"},
+      {"YES"},
+      {"y"},
+      {"Y"},
+    };
+  }
+
+  @DataProvider
+  public Object[][] booleanFalseValues() {
+    return new Object[][]{
+      {"false"},
+      {"f"},
+      {"0"},
+      {"off"},
+      {"no"},
+      {"n"},
+      {""},
+      {"any other value"},
+    };
+  }
+
+  @DataProvider
+  public Object[][] primitiveTypesWithNullValues() {
+    return new Object[][]{
+      {boolean.class, false},
+      {byte.class, (byte) 0},
+      {short.class, (short) 0},
+      {int.class, 0},
+      {long.class, 0L},
+      {float.class, 0F},
+      {double.class, 0D},
+      {char.class, (char) 0},
+    };
+  }
+
+  @DataProvider
+  public Object[][] boxedTypesWithNullValues() {
+    return new Object[][]{
+      {Boolean.class},
+      {Byte.class},
+      {Short.class},
+      {Integer.class},
+      {Long.class},
+      {Float.class},
+      {Double.class},
+      {Character.class},
+    };
+  }
 
   @Test
   public void parseStrToGenericType__string() {
@@ -47,7 +130,82 @@ public class ParseUtilTest {
     //
     //
 
-    //noinspection ConstantValue
+    assertThat(value).isNull();
+  }
+
+  @Test(dataProvider = "primitiveAndBoxedTypes")
+  public void parseStrToGenericType__primitive_and_boxed_types(String valueStr, Type type, Object expectedValue) {
+
+    DynamicParamsFake dynamicParams = new DynamicParamsFake(13);
+
+    //
+    //
+    Object value = ParseUtil.parseStrToGenericType(valueStr, dynamicParams, type);
+    //
+    //
+
+    assertThat(value).isEqualTo(expectedValue);
+    assertThat(value).isInstanceOf(expectedValue.getClass());
+  }
+
+  @Test(dataProvider = "booleanTrueValues")
+  public void parseStrToGenericType__boolean_true_values(String valueStr) {
+
+    DynamicParamsFake dynamicParams = new DynamicParamsFake(13);
+
+    //
+    //
+    Object primitiveValue = ParseUtil.parseStrToGenericType(valueStr, dynamicParams, boolean.class);
+    Object boxedValue     = ParseUtil.parseStrToGenericType(valueStr, dynamicParams, Boolean.class);
+    //
+    //
+
+    assertThat(primitiveValue).isEqualTo(true);
+    assertThat(boxedValue).isEqualTo(true);
+  }
+
+  @Test(dataProvider = "booleanFalseValues")
+  public void parseStrToGenericType__boolean_false_values(String valueStr) {
+
+    DynamicParamsFake dynamicParams = new DynamicParamsFake(13);
+
+    //
+    //
+    Object primitiveValue = ParseUtil.parseStrToGenericType(valueStr, dynamicParams, boolean.class);
+    Object boxedValue     = ParseUtil.parseStrToGenericType(valueStr, dynamicParams, Boolean.class);
+    //
+    //
+
+    assertThat(primitiveValue).isEqualTo(false);
+    assertThat(boxedValue).isEqualTo(false);
+  }
+
+  @Test(dataProvider = "primitiveTypesWithNullValues")
+  public void parseStrToGenericType__primitive_types__null_value(Type type, Object expectedValue) {
+
+    DynamicParamsFake dynamicParams = new DynamicParamsFake(13);
+
+    //
+    //
+    Object value = ParseUtil.parseStrToGenericType(null, dynamicParams, type);
+    //
+    //
+
+    assertThat(value).isEqualTo(expectedValue);
+    assertThat(value).isInstanceOf(expectedValue.getClass());
+  }
+
+  @Test(dataProvider = "boxedTypesWithNullValues")
+  public void parseStrToGenericType__boxed_types__null_value(Type type) {
+
+    DynamicParamsFake dynamicParams = new DynamicParamsFake(13);
+
+    //
+    //
+    Object value = ParseUtil.parseStrToGenericType(null, dynamicParams, type);
+    //
+    //
+
     assertThat(value).isNull();
   }
 }
